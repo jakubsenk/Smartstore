f4f0c3010a40604ae505166d18922f0d1e61280f
jakubsenk <29975881+jakubsenk@users.noreply.github.com>
2024-08-24 15:35:27 +0200

    PayU module

---
+++ src/Smartstore.Modules/Smartstore.PayU/Providers/PayUProvider.cs
@@ -0,0 +1,156 @@
+﻿using Smartstore.Core;
+using Smartstore.Core.Checkout.Cart;
+using Smartstore.Core.Checkout.Orders;
+using Smartstore.Core.Checkout.Payment;
+using Smartstore.Core.Common.Services;
+using Smartstore.Core.Configuration;
+using Smartstore.Core.Stores;
+using Smartstore.Core.Widgets;
+using Smartstore.Engine.Modularity;
+using Smartstore.Http;
+using Smartstore.PayU.Configuration;
+using Smartstore.PayU.Controllers;
+using Smartstore.PayU.Models;
+using Smartstore.PayU.Services;
+
+namespace Smartstore.PayU.Providers
+{
+    [SystemName("Payments.PayU")]
+    [FriendlyName("PayU")]
+    [Order(1)]
+    public class PayUProvider : PaymentMethodBase, IConfigurable
+    {
+        public override PaymentMethodType PaymentMethodType => PaymentMethodType.Redirection;
+        public override bool RequiresInteraction => true;
+        public override bool SupportVoid => true;
+        public override bool SupportRefund => true;
+        public override bool SupportPartiallyRefund => true;
+        public override bool RequiresPaymentSelection => false;
+
+        private readonly ICheckoutStateAccessor _checkoutStateAccessor;
+        private readonly IStoreContext _storeContext;
+        private readonly ISettingFactory _settingFactory;
+        private readonly ICommonServices _services;
+        private readonly IShoppingCartService _shoppingCartService;
+        private readonly IOrderCalculationService _orderCalculationService;
+        private readonly ICurrencyService _currencyService;
+        private readonly IRoundingHelper _roundingHelper;
+        private readonly PayUGateService _payService;
+        public static string SystemName => "Payments.PayU";
+
+        private PayUPaymentResult payResult;
+
+        public PayUProvider(
+                    ICommonServices services,
+                    IStoreContext storeContext,
+                    IShoppingCartService shoppingCartService,
+                    IOrderCalculationService orderCalculationService,
+                    ICheckoutStateAccessor checkoutStateAccessor,
+                    ISettingFactory settingFactory,
+                    ICurrencyService currencyService,
+                    IRoundingHelper roundingHelper,
+                    PayUGateService payService
+                    )
+        {
+            _services = services;
+            _storeContext = storeContext;
+            _shoppingCartService = shoppingCartService;
+            _orderCalculationService = orderCalculationService;
+            _checkoutStateAccessor = checkoutStateAccessor;
+            _settingFactory = settingFactory;
+            _payService = payService;
+            _currencyService = currencyService;
+            _roundingHelper = roundingHelper;
+        }
+
+        public RouteInfo GetConfigurationRoute()
+            => new(nameof(PayUAdminController.Configure), "PayUAdmin", new { area = "Admin" });
+
+        public override Widget GetPaymentInfoWidget() => null;
+
+        public override async Task<(decimal FixedFeeOrPercentage, bool UsePercentage)> GetPaymentFeeInfoAsync(ShoppingCart cart)
+        {
+            PayUSettings settings = await _settingFactory.LoadSettingsAsync<PayUSettings>(_storeContext.CurrentStore.Id);
+
+            return (settings.AdditionalFee, settings.AdditionalFeePercentage);
+        }
+
+
+        public override async Task<ProcessPaymentResult> ProcessPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+        {
+            if (processPaymentRequest.OrderGuid == Guid.Empty)
+            {
+                throw new Exception($"{nameof(processPaymentRequest.OrderGuid)} is missing.");
+            }
+
+            ProcessPaymentResult result = new ProcessPaymentResult();
+
+
+            PayUPaymentRequest request = await _payService.GetPayUPaymentRequestAsync(processPaymentRequest);
+
+            payResult = await _payService.InitializePayment(request);
+
+            result.NewPaymentStatus = PaymentStatus.Authorized;
+            result.AuthorizationTransactionCode = payResult.TransactionID;
+            result.AuthorizationTransactionId = payResult.TransactionID;
+
+            return result;
+        }
+
+        public override Task<PostProcessPaymentRequest> PostProcessPaymentAsync(PostProcessPaymentRequest postProcessPaymentRequest)
+        {
+            postProcessPaymentRequest.RedirectUrl = payResult.RedirectUri;
+            postProcessPaymentRequest.IsRePostProcessPayment = false;
+            return Task.FromResult(postProcessPaymentRequest);
+        }
+
+        public override async Task<VoidPaymentResult> VoidAsync(VoidPaymentRequest voidPaymentRequest)
+        {
+            Order order = voidPaymentRequest.Order;
+            VoidPaymentResult result = new VoidPaymentResult
+            {
+                NewPaymentStatus = order.PaymentStatus
+            };
+
+            if (order.PaymentStatus == PaymentStatus.Paid)
+            {
+                return result;
+            }
+
+            if (await _payService.VoidPayment(order.AuthorizationTransactionId))
+            {
+                result.NewPaymentStatus = PaymentStatus.Voided;
+            }
+            return result;
+        }
+
+        public override async Task<RefundPaymentResult> RefundAsync(RefundPaymentRequest refundPaymentRequest)
+        {
+            Order order = refundPaymentRequest.Order;
+            RefundPaymentResult result = new RefundPaymentResult
+            {
+                NewPaymentStatus = order.PaymentStatus,
+            };
+
+            if (order.PaymentStatus != PaymentStatus.Paid)
+            {
+                return result;
+            }
+
+            PayURefundResult refundResult;
+            if (refundPaymentRequest.IsPartialRefund)
+            {
+                refundResult = await _payService.RefundPayment(order.AuthorizationTransactionId, refundPaymentRequest.AmountToRefund);
+            }
+            else
+            {
+                refundResult = await _payService.RefundPayment(order.AuthorizationTransactionId, null);
+            }
+            if (refundResult.Success)
+            {
+                result.NewPaymentStatus = refundPaymentRequest.IsPartialRefund ? PaymentStatus.PartiallyRefunded : PaymentStatus.Refunded;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Smartstore.Modules/Smartstore.PayU/Services/PayUGateAuthorizationProvider.cs
@@ -0,0 +1,96 @@
+﻿using System.Net.Http.Headers;
+using System.Net;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using System.Net.Http;
+using Smartstore.PayU.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Smartstore.PayU.Services
+{
+    public class PayUGateAuthorizationService
+    {
+        private string endpoint;
+        private string region;
+        private string posid;
+        private string clientid;
+        private string clientsecret;
+
+        private string accessToken;
+        private DateTime? validUntil = null;
+
+        public ILogger Logger { get; set; } = NullLogger.Instance;
+
+        public PayUGateAuthorizationService(PayUSettings config)
+        {
+            UpdateConfiguration(config);
+        }
+
+        public void UpdateConfiguration(PayUSettings config)
+        {
+            endpoint = config.IsSandbox ? config.SandboxEndpoint : config.Endpoint;
+            region = config.IsSandbox ? config.SandboxRegion : config.Region;
+            posid = config.IsSandbox ? config.SandboxPosID : config.PosID;
+            clientid = config.IsSandbox ? config.SandboxClientID : config.ClientID;
+            clientsecret = config.IsSandbox ? config.SandboxClientSecret : config.ClientSecret;
+        }
+
+        private async Task GetAccessTokenInternal()
+        {
+            if (string.IsNullOrEmpty(endpoint) ||
+                string.IsNullOrEmpty(region) ||
+                string.IsNullOrEmpty(posid) ||
+                string.IsNullOrEmpty(clientid) ||
+                string.IsNullOrEmpty(clientsecret))
+            {
+                throw new InvalidOperationException("PayU configuration is missing.");
+            }
+
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.AllowAutoRedirect = false;
+            using (HttpClient hc = new HttpClient(httpClientHandler, true))
+            {
+                HttpContent content = new FormUrlEncodedContent(
+                [
+                    new KeyValuePair<string, string>("grant_type", "client_credentials"),
+                    new KeyValuePair<string, string>("client_id", clientid),
+                    new KeyValuePair<string, string>("client_secret", clientsecret),
+                ]);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+                HttpResponseMessage response = await hc.PostAsync($"{endpoint}/{region}/standard/user/oauth/authorize", content);
+
+                string result = await response.Content.ReadAsStringAsync();
+
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    Logger.Error("Received non ok response during authorization.");
+                    Logger.Error(result);
+                    throw new ApplicationException("Received non ok response during authorization.");
+                }
+
+                JsonNode data = JsonSerializer.Deserialize<JsonNode>(result);
+                accessToken = data["access_token"].GetValue<string>();
+                string tokenType = data["token_type"].GetValue<string>();
+                int secondsValid = data["expires_in"].GetValue<int>();
+                validUntil = DateTime.Now.AddSeconds(secondsValid - 10);
+
+                if (string.IsNullOrEmpty(accessToken) || tokenType != "bearer")
+                {
+                    Logger.Error("Received invalid token, original response:");
+                    Logger.Error(result);
+                    throw new ApplicationException("Received invalid token.");
+                }
+            }
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            if (validUntil == null || accessToken == null || validUntil < DateTime.Now)
+            {
+                await GetAccessTokenInternal();
+            }
+            return accessToken;
+        }
+    }
+}
