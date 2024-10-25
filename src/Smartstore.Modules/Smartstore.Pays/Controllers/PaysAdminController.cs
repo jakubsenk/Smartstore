10125d07ed0a48227527497e67d7d906fed6a3e6
jakubsenk <29975881+jakubsenk@users.noreply.github.com>
2024-10-25 16:30:44 +0200

    pays module

---
+++ src/Smartstore.Modules/Smartstore.Pays/Controllers/PaysAdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Smartstore.ComponentModel;
+using Smartstore.Core.Security;
+using Smartstore.Engine.Modularity;
+using Smartstore.Pays.Configuration;
+using Smartstore.Pays.Models;
+using Smartstore.Pays.Providers;
+using Smartstore.Web.Controllers;
+using Smartstore.Web.Modelling.Settings;
+
+namespace Smartstore.Pays.Controllers
+{
+    [Area("Admin")]
+    public class PaysAdminController : ModuleController
+    {
+        private readonly IProviderManager _providerManager;
+
+        public PaysAdminController(IProviderManager providerManager)
+        {
+            _providerManager = providerManager;
+        }
+
+        [LoadSetting, AuthorizeAdmin]
+        public IActionResult Configure(PaysSettings settings)
+        {
+            ViewBag.Provider = _providerManager.GetProvider(PaysProvider.SystemName).Metadata;
+
+            ConfigurationModel model = MiniMapper.Map<PaysSettings, ConfigurationModel>(settings);
+
+            ViewBag.CurrentCurrencyCode = Services.CurrencyService.PrimaryCurrency.CurrencyCode;
+
+            return View(model);
+        }
+
+        [HttpPost, SaveSetting, AuthorizeAdmin]
+        public IActionResult Configure(ConfigurationModel model, PaysSettings settings)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Configure(settings);
+            }
+
+            ModelState.Clear();
+            MiniMapper.Map(model, settings);
+
+            return RedirectToAction(nameof(Configure));
+        }
+    }
+}
---
+++ src/Smartstore.Modules/Smartstore.Pays/Module.cs
@@ -0,0 +1,65 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+using Smartstore.Core.Checkout.Payment;
+using Smartstore.Core.Identity;
+using Smartstore.Core.Localization;
+using Smartstore.Engine.Modularity;
+using Smartstore.Http;
+using Smartstore.Pays.Configuration;
+using Smartstore.Pays.Providers;
+
+namespace Smartstore.Pays
+{
+    internal class Module : ModuleBase, IConfigurable, ICookiePublisher
+    {
+        private readonly IPaymentService _paymentService;
+
+        public Module(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        public Localizer T { get; set; } = NullLocalizer.Instance;
+
+        public RouteInfo GetConfigurationRoute()
+            => new("Configure", "PaysAdmin", new { area = "Admin" });
+
+        public async Task<IEnumerable<CookieInfo>> GetCookieInfosAsync()
+        {
+            var store = Services.StoreContext.CurrentStore;
+            bool isActiveStripe = await _paymentService.IsPaymentProviderActiveAsync(PaysProvider.SystemName, null, store.Id);
+
+            if (isActiveStripe)
+            {
+                CookieInfo cookieInfo = new CookieInfo
+                {
+                    Name = T("Plugins.FriendlyName.Smartstore.Pays"),
+                    Description = T("Plugins.Smartstore.Pays.CookieInfo"),
+                    CookieType = CookieType.Required
+                };
+
+                return new List<CookieInfo> { cookieInfo }.AsEnumerable();
+            }
+
+            return null;
+        }
+
+        public override async Task InstallAsync(ModuleInstallationContext context)
+        {
+            await ImportLanguageResourcesAsync();
+            await TrySaveSettingsAsync<PaysSettings>();
+
+            await base.InstallAsync(context);
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await DeleteLanguageResourcesAsync();
+            await DeleteSettingsAsync<PaysSettings>();
+
+            await base.UninstallAsync();
+        }
+    }
+}
